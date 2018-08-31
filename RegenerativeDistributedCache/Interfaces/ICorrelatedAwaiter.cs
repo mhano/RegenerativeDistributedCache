06bc6eee4eed4109a219f085e61b91493aef9727
Mhano Harkness <28881779+mhano@users.noreply.github.com>
2018-08-31 12:52:50 +1000

    final missing code doc comments, removed auto generated explicit binding redirect (from Tests project) that wasn't needed and caused build warning message.

--- RegenerativeDistributedCache/Interfaces/ICorrelatedAwaiter.cs
+++ RegenerativeDistributedCache/Interfaces/ICorrelatedAwaiter.cs
@@ -10,7 +10,16 @@ namespace RegenerativeDistributedCache.Interfaces
     /// <typeparam name="TMessage"></typeparam>
     public interface ICorrelatedAwaiter<TMessage> : IDisposable
     {
+        /// <summary>
+        /// Cancel an awaiter ahead of disposing it. This will set the task completion
+        /// result to cancelled if it isn't already complete/errored.
+        /// </summary>
         void Cancel();
+
+        /// <summary>
+        /// Task which can be waited on (e.g. via await, .Wait() or .Result).
+        /// Task result is of TMessage which was send to NotifyAwaiters.
+        /// </summary>
         Task<TMessage> Task { get; }
     }
 }
